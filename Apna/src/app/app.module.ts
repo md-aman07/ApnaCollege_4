2a449f1a29e85a10f9cbdc265977bfa643a29a82
md-aman07 <mohdaman21426@gmail.com>
2025-08-10 23:42:06 +0530

    add first

---
+++ Apna/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
